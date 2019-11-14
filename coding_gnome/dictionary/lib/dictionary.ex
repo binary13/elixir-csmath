89e8e1163b1349a652aab8c63b8a82829dbf17b4
Justin Neil <justinmneil@gmail.com>
2019-11-14 14:13:52 -0500

    Testing ElixirLS

--- coding_gnome/dictionary/lib/dictionary.ex
+++ coding_gnome/dictionary/lib/dictionary.ex
@@ -12,7 +12,17 @@ defmodule Dictionary do
       :world
 
   """
-  def hello do
-    :world
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    list = String.split(contents)
   end
+
+  def random_word do
+    Enum.random(word_list())
+  end
+
+  def test do
+    IO.puts "Testing! Testing! 1, 2, 3!"
+  end
+
 end
