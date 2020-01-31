90cfbee93e142ad994f37300d86ccf96c9b41c42
Justin Neil <justinmneil@gmail.com>
2020-01-31 09:10:34 -0500

    Refactored Euler23

--- Euler23.exs
+++ Euler23.exs
@@ -4,55 +4,60 @@
 #
 ###############################################################################
 
-# A perfect number is a number for which the sum of its proper divisors is 
-# exactly equal to the number. For example, the sum of the proper divisors of 28 
+# A perfect number is a number for which the sum of its proper divisors is
+# exactly equal to the number. For example, the sum of the proper divisors of 28
 # would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
 
-# A number n is called deficient if the sum of its proper divisors is less than n 
+# A number n is called deficient if the sum of its proper divisors is less than n
 # and it is called abundant if this sum exceeds n.
 
-# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest 
-# number that can be written as the sum of two abundant numbers is 24. By 
-# mathematical analysis, it can be shown that all integers greater than 28123 can 
-# be written as the sum of two abundant numbers. However, this upper limit cannot 
-# be reduced any further by analysis even though it is known that the greatest 
-# number that cannot be expressed as the sum of two abundant numbers is less than 
+# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest
+# number that can be written as the sum of two abundant numbers is 24. By
+# mathematical analysis, it can be shown that all integers greater than 28123 can
+# be written as the sum of two abundant numbers. However, this upper limit cannot
+# be reduced any further by analysis even though it is known that the greatest
+# number that cannot be expressed as the sum of two abundant numbers is less than
 # this limit.
 
-# Find the sum of all the positive integers which cannot be written as the sum of 
+# Find the sum of all the positive integers which cannot be written as the sum of
 # two abundant numbers.
 
 defmodule Euler23 do
-	
-	def go do
-		generate_ubundant_numbers(1, [], 28123)
-		|> filter_nonsum_ints
+
+	# def go do
+		# generate_abundant_numbers(1, [], 28123)
+		# |> filter_nonsum_ints
 		#|> Enum.sum
+	# end
+
+	def is_abundant?(num) do
+		num < sum_prop_divisors(num)
 	end
 
-	def generate_ubundant_numbers(num, list, limit) do
-		cond do
-			num > limit -> list
-			num < sum_prop_divisors(num) -> generate_ubundant_numbers(num + 1, list ++ [num], limit)
-			true -> generate_ubundant_numbers(num + 1, list, limit)
-		end
-	end
+	# Doesn't work - I don't need a list of abundant numbers
+	# def generate_abundant_numbers(num, list, limit) do
+		# cond do
+			# num > limit -> list
+			# num < sum_prop_divisors(num) -> generate_abundant_numbers(num + 1, list ++ [num], limit)
+			# true -> generate_abundant_numbers(num + 1, list, limit)
+		# end
+	# end
 
 	def sum_prop_divisors(num) do
-		factor(num)
+		divisors(num)
 		|> Enum.sum
 	end
 
-	def factor(num) do
+	def divisors(num) do
 	    find_factors(num)
 	end
-	  
+
 	defp find_factors(num, divisor \\ 1, factors \\ [])
-	  
+
 	defp find_factors(num, divisor, factors) when divisor > num/2 do
 	    Enum.sort(factors)
 	end
-	  
+
 	defp find_factors(num, divisor, factors) do
 	    if rem(num, divisor) == 0 do
 	      find_factors(num, divisor+1, factors ++ [divisor])
@@ -62,6 +67,6 @@ defmodule Euler23 do
 	end
 
 	def filter_nonsum_ints(list) do
-		
+
 	end
 end
