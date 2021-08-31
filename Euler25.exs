152484d766916cb15bf2cdc65a37ccb59639c134
Justin Neil <justinmneil@gmail.com>
2021-08-31 15:26:49 -0400

    Started Euler26

--- Euler25.exs
+++ Euler25.exs
@@ -17,7 +17,36 @@
 # F12 = 144
 # The 12th term, F12, is the first term to contain three digits.
 
-# What is the index of the first term in the Fibonacci sequence to 
+# What is the index of the first term in the Fibonacci sequence to
 # contain 1000 digits?
 
-defmodule
+defmodule Euler25 do
+    def seq(0), do: 0
+    def seq(1), do: 1
+    def seq(n) when n > 1 do
+        compute_seq(n, 1, [0, 1])
+    end
+
+    defp compute_seq(n, i, acc) when n == i do
+        Enum.at(acc, length(acc)-1)
+    end
+
+    defp compute_seq(n, i, acc) do
+        len = length(acc)
+        compute_seq(n, i+1, acc ++ [Enum.at(acc, len - 1) + Enum.at(acc, len-2)])
+    end
+
+    def fib_digits(num \\ 0) do
+        seq(num)
+        |> Integer.digits()
+        |> length()
+    end
+
+    def thousand(num \\ 0) do
+        if fib_digits(num) == 1000 do
+            num
+        else
+            thousand(num+1)
+        end
+    end
+end
--- fibonacci.exs
+++ fibonacci.exs
@@ -1,11 +1,24 @@
 defmodule Fibonacci do
-    def fib(num) do
+    def fib_recursive(num) do
         case num do
             0 -> 0
             1 -> 1
-            _ -> fib(num-1) + fib(num-2)
+            _ -> fib_recursive(num-1) + fib_recursive(num-2)
         end
     end
-end
 
-IO.puts(Fibonacci.fib(40))
+    def fib_iterative(0), do: 0
+    def fib_iterative(1), do: 1
+    def fib_iterative(n) when n > 1 do
+        compute_seq(n, 1, [0, 1])
+    end
+
+    defp compute_seq(n, i, acc) when n == i do
+        Enum.at(acc, length(acc)-1)
+    end
+
+    defp compute_seq(n, i, acc) do
+        len = length(acc)
+        compute_seq(n, i+1, acc ++ [Enum.at(acc, len - 1) + Enum.at(acc, len-2)])
+    end
+end
